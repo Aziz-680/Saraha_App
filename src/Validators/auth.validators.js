607e43dc606d4332dec8da9a1376a4e0229216f8
Aziz-680 <abdelazizhisham8@gmail.com>
2026-05-10 16:26:59 +0300

    10_General_Validators

--- src/Validators/auth.validators.js
+++ src/Validators/auth.validators.js
@@ -1,35 +1,22 @@
 import Joi from "joi";
 import { GENDER } from "../Common/index.js";
+import { generalValidators } from "../Common/validators/general.validators.js";
 
 export const registerSchema = {
     body: Joi.object({
         firstName: Joi.string().alphanum(),
         lastName: Joi.string().min(2).max(50),
-        email: Joi
-        .string()
-        .email({ tlds: { allow: ['com', 'org'] } , multiple:true , separator:'$'})
-            .messages({
-                'string.pattern.base': 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
-                'any.required': 'Password cannot be empty'
-            }),
-        password: Joi
-        .string()
-        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
-            .messages({
-                'string.pattern.base': 'Phone number must contain only numbers.',
-                'string.length': 'Phone number must be exactly 11 digits long.'
-            }),
+        email: generalValidators.email,
+        password: generalValidators.password,
         confirmPassword: Joi.valid( Joi.ref('password')),
         gender: Joi.string().valid(...Object.values(GENDER)),
         phone: Joi.string().length(11),
         isBoolean : Joi.boolean().truthy('yes').falsy('no').sensitive(true),
         date: Joi.date().greater(Date.now()),
+        skills: Joi.array().ordered(Joi.string() , Joi.number()).required(),
+        userId:generalValidators._id
     })
     .with('email' , 'password')
-    .with('password' , 'confirmPassword')
+    .with('password', 'confirmPassword')
     // .options({presence:'optional'})
 }
-
-
-
-
